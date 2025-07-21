b57c3525e509114ef42c906d4c8b4368392506a8
Arthur <arthur.perez66000@gmail.com>
2025-07-21 10:59:33 +0200

    ex05 done

---
+++ CPP01/ex05/main.cpp
@@ -0,0 +1,18 @@
+#include "Harl.hpp"
+
+int main()
+{
+    Harl harl;
+
+    harl.complain("DEBUG");
+
+    harl.complain("");
+
+    harl.complain("INFO");
+
+    harl.complain("WARNING");
+
+    harl.complain("WARNINGG");
+
+    harl.complain("ERROR");
+}
