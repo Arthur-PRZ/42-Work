81296ef8f43a17f0db4090fed892283449c6cb34
Arthur <arthur.perez66000@gmail.com>
2025-07-21 09:32:50 +0200

    ex04 done

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -7,29 +7,38 @@ static void getNewFile(std::ifstream& file, std::ofstream& newFile,
 {
     std::string line;
     std::string goodLine;
-    int         index;
+    size_t      index;
 
     while (std::getline(file, line))
     {
         index = line.find(replaced);
-        line.erase(index, line.length());
-        line.insert(index, replace);
+        while (index != std::string::npos)
+        {
+            line.erase(index, replaced.length());
+            line.insert(index, replace);
+            index = line.find(replaced);
+        }
         newFile << line << std::endl;
     }
 }
 
 int main(int argc, char **argv)
 {
-    if (argc != 3)
+    if (argc < 4)
     {
         std::cout << "Error : tree parameters needed : filename, string replaced, new string" << std::endl;
+        return 1;
+    }
+    else if (argc > 4)
+    {
+        std::cout << "Error : Only tree parameters needed : filename, string replaced, new string" << std::endl;
         return 1;
     }
     std::string fileName = argv[1];
     std::string newFile = fileName + ".replace";
 
     std::ifstream file(argv[1]);
-    std::ofstream new_file(newFile);
+    std::ofstream new_file(newFile.c_str());
     if (!file.is_open() || !new_file.is_open())
     {
         std::cout << "Error : Can't open the file" << std::endl;
