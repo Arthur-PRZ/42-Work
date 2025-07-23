97e04ab2cc4b37c69c14c21aeb2d911469bdb266
Arthur <arthur.perez66000@gmail.com>
2025-07-23 11:30:08 +0200

    ex01 done

---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+
+class Fixed
+{
+    private :
+
+    int value;
+    static const int nbrFracionalBits = 8;
+
+    public :
+
+    Fixed();
+    Fixed(const Fixed& other);
+    Fixed(const int& other);
+    Fixed(const float& other);
+    Fixed& operator=(const Fixed& fixed);
+    ~Fixed();
+    int     toInt( void ) const;
+    int     getRawBits( void ) const;
+    void    setRawBits( int const raw );
+    float   toFloat( void ) const;
+};
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed);
