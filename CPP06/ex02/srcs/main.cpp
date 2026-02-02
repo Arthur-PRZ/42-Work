26b70fd8eb0b2fe8b5ff45e0c3cca1736517389d
Arthur <arthur66500@gmail.com>
2026-02-02 17:31:20 +0000

    ex02 done

--- CPP06/ex02/srcs/main.cpp
+++ CPP06/ex02/srcs/main.cpp
@@ -4,10 +4,11 @@
 #include "../incs/Base.hpp"
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 
 Base* generate(void)
 {
-    std::srand(std::time(nullptr));
+    std::srand(std::time(0));
     int r = std::rand() % 3;
 
     if (r == 0)
@@ -20,9 +21,51 @@ Base* generate(void)
 
 void identify(Base *p)
 {
-    
+    if (dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
 }
+
+void identify(Base &p)
+{
+    try
+    {
+        dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+        return ;
+    }
+    catch (...)
+    {
+    }
+    try
+    {
+        dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+        return ;
+    }
+    catch (...)
+    {
+    }
+    try
+    {
+        dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+        return ;
+    }
+    catch (...)
+    {
+    }
+}
+
 int main()
 {
+    Base *base;
 
+    base = generate();
+    identify(base);
+    identify(*base);
+    delete base;
 }
