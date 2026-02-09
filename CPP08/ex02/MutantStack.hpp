5629887d75dc4e76482c72c19eb43f7a4c25d19b
Arthur <arthur.perez66000@gmail.com>
2026-02-09 09:34:55 +0100

    change main

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -20,10 +20,10 @@ class MutantStack : public std::stack<T>
     ~MutantStack() {}
 
     typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
     iterator begin() { return this->c.begin(); }
     iterator end() { return this->c.end(); }
-
-    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
     const_iterator begin() const { return this->c.begin(); }
     const_iterator end() const { return this->c.end(); }
 };
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -8,36 +8,44 @@ int main()
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
-    std::cout << "Top: " << mstack.top() << std::endl;
     mstack.pop();
-    std::cout << "Size after pop: " << mstack.size() << std::endl;
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
     mstack.push(0);
 
-    std::cout << "Elements (MutantStack):" << std::endl;
-    for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it)
-        std::cout << *it << std::endl;
+    std::cout << "Elements MutantStack:" << std::endl;
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
 
-    std::stack<int> s(mstack);
-    std::cout << "Copy to std::stack works, top: " << s.top() << std::endl;
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
 
     std::cout << "\n=== Test std::list ===" << std::endl;
     std::list<int> lst;
     lst.push_back(5);
     lst.push_back(17);
-    std::cout << "Back: " << lst.back() << std::endl;
     lst.pop_back();
-    std::cout << "Size after pop: " << lst.size() << std::endl;
     lst.push_back(3);
     lst.push_back(5);
     lst.push_back(737);
     lst.push_back(0);
 
-    std::cout << "Elements (std::list):" << std::endl;
-    for (std::list<int>::iterator it = lst.begin(); it != lst.end(); ++it)
-        std::cout << *it << std::endl;
+    std::cout << "Elements list:" << std::endl;
+    std::list<int>::iterator lit = lst.begin();
+    std::list<int>::iterator lite = lst.end();
 
+    ++lit;
+    --lit;
+    while (lit != lite)
+    {
+        std::cout << *lit << std::endl;
+        ++lit;
+    }
     return 0;
 }
