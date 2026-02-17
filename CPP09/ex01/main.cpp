5e4fd58b5f140ccc4a98aad2d8729ed31bac10e2
Arthur <arthur.perez66000@gmail.com>
2026-02-17 10:17:50 +0100

    error input argc handle

--- CPP09/ex01/main.cpp
+++ CPP09/ex01/main.cpp
@@ -2,10 +2,14 @@
 
 int main(int argc, char **argv)
 {
+    if (argc != 2)
+    {
+        std::cerr << "Usage: ./RPN \"3 4 + 5 -\"" << std::endl;
+        return 1;
+    }
     RPN rpn;
     std::string str(argv[1]);
 
-    (void)argc;
     try
     {
         std::cout << rpn.calculate(str) << std::endl;
